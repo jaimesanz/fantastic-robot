57b535590cae3a6bda7fc1e7e7d1eddee1eefce2
jaimesanz <jimbosanz23@gmail.com>
2017-12-30 16:08:26 -0300

    Home url shows news from emol.cl

--- fantastic_robot/news/bots.py
+++ fantastic_robot/news/bots.py
@@ -3,15 +3,19 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 import time
-from abc import ABC, abstractmethod
-from urllib import request
+from abc import ABC, abstractmethod, abstractclassmethod
 
 
 class BaseBot(ABC):
     """Base class for defining Bots that scrape newspaper web pages."""
 
-    @abstractmethod
-    def get_source_url(self):
+    soup = None
+
+    def __init__(self, soup):
+        self.soup = soup
+
+    @abstractclassmethod
+    def get_source_url(cls):
         """Returns url that will be used to fetch the data."""
         pass
 
@@ -20,8 +24,8 @@ class BaseBot(ABC):
         """Returns name of the newspaper."""
         pass
 
-    @abstractmethod
-    def fetch(self):
+    @abstractclassmethod
+    def fetch(cls):
         """Makes request to source's url to fetch data."""
         pass
 
@@ -37,21 +41,22 @@ class EmolBot(BaseBot):
     def get_source_name(self):  # noqa
         return "EL Mercurio"
 
-    def get_source_url(self):  # noqa
+    @classmethod
+    def get_source_url(cls):  # noqa
         return "http://www.emol.com/noticias/"
 
-    def fetch(self):
+    @classmethod
+    def fetch(cls):
         driver = webdriver.PhantomJS()
-        driver.get(self.get_source_url())
+        driver.get(cls.get_source_url())
         time.sleep(5)
         html_source = driver.page_source
         soup = BeautifulSoup(html_source, "html.parser")
 
-        return soup
+        return cls(soup=soup)
 
     def parse(self):
-        soup = self.fetch()
-        lis = soup.find(id='listNews').find_all('li')
+        lis = self.soup.find(id='listNews').find_all('li')
 
         parsed_data = []
 
@@ -61,10 +66,11 @@ class EmolBot(BaseBot):
             bajada = li.find(id="BajadaNoticia").string
             link = li.find(id="LinkNoticia")
             parsed_data.append({
-                "time": _time,
-                "category": category,
+                "title": link.string,
                 "bajada": bajada,
+                "category": category,
+                "time": _time,
                 "url": link["href"],
-                "title": link.string
+                "source": self.get_source_name()
             })
         return parsed_data
--- fantastic_robot/news/views.py
+++ fantastic_robot/news/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
+from .bots import EmolBot
+
 
 def home(request):
-    return render(request, 'news/home.html', {})
+    emol = EmolBot.fetch()
+    print(emol.parse())
+    return render(request, 'news/home.html', {
+        "data": emol.parse()
+    })
