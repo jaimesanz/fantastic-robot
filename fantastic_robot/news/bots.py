a5f87b56562f36f98464f4755f588a44ab3c26fc
jaimesanz <jimbosanz23@gmail.com>
2017-12-30 14:59:04 -0300

    Override abstract methods of BaseBot.

--- fantastic_robot/news/bots.py
+++ fantastic_robot/news/bots.py
@@ -7,22 +7,37 @@ class BaseBot(ABC):
 
     @abstractmethod
     def get_source_url(self):
+        """Returns url that will be used to fetch the data."""
         pass
 
     @abstractmethod
     def get_source_name(self):
+        """Returns name of the newspaper."""
         pass
 
     @abstractmethod
     def fetch(self):
+        """Makes request to source's url to fetch data."""
         pass
 
     @abstractmethod
     def parse(self):
+        """Parses the result of the 'fetch' command into a list of JSON data."""
         pass
 
 
 class EmolBot(BaseBot):
     """Bot that reads and parses news from EMOL."""
 
-    pass
+    def get_source_name(self):  # noqa
+        return "EL Mercurio"
+
+    def get_source_url(self):  # noqa
+        return "http://www.emol.com/noticias/"
+
+    def fetch(self):
+        print("wena")
+        return "asf"
+
+    def parse(self):
+        return "sdf"
